60146a61b1ea760fd73db53507bd22922e10f7a2
Kris Rowe <kdrowe@google.com>
2019-06-20 19:03:52 +0000

    Make fully parameterized with single test plan.

--- run.sh
+++ run.sh
@@ -1,25 +1,69 @@
 #!/bin/bash
 rm -rf output/ 
 
-#{__P(VariableName)}{__P(VariableName)}{__P(VariableName)}{__P(VariableName)}{__P(VariableName)}{__P(VariableName)}sdfsdf Warm-up the Apigee demo env service.
-#curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' "http://amer-demo22-prod.apigee.net/testgetapi"  --header "x-apikey: NIjCAEZCKgclx5qxVyY60WRggM6v0Cb7"
 
-# Warm-up the Apigee prod env service.
-#curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' "http://geico-prod.apigee.net/testgetapi"  --header "x-apikey: upYgIDoREuHk3XGXeH8DweIN3J4ZfDQ5"
+# Check the Apigee prod env service pointing to Google Cloud Run
+export APIHOST="http://geico-prod.apigee.net/testgetapi"
+export APIHEADER="x-apikey: upYgIDoREuHk3XGXeH8DweIN3J4ZfDQ5"
+curl $APIHOST --header "${APIHEADER}"
+# Repeat and show response time.
+curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' $APIHOST --header "${APIHEADER}"
 
-# Warm-up the Azure APIM env.
-#curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' "http://apim-test-instance-001.azure-api.net/api/employee/getpoliciesAWS" --header "Ocp-Apim-Subscription-Key:  314f53c3f66340ca912cdcba567364a9"
+# Check the Apigee prod env service pointing to Azure Containers
+export APIHOST="http://geico-prod.apigee.net/hello-from-gates"
+export APIHEADER="x-apikey: upYgIDoREuHk3XGXeH8DweIN3J4ZfDQ5"
+curl $APIHOST --header "${APIHEADER}"
+# Repeat and show response time.
+curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' $APIHOST --header "${APIHEADER}"
 
-#cp apigee.jmx azure-apim.jmx
+# Check the Azure API pointing to Google Cloud Run 
+export APIHOST="https://perf-poc-eastus2-01.regional.azure-api.net/test/"
+export APIHEADER="Ocp-Apim-Subscription-Key: a9fecf7d693641ccb57211f2720405c6"
+curl $APIHOST --header "${APIHEADER}"
+# Repeat and show response time.
+curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' $APIHOST --header "${APIHEADER}"
 
-#sed 's/amer-demo22-prod.apigee.net/perf-poc-eastus2-01.regional.azure-api.net/g' azure-apim.jmx > azure-apim.jmx
-#sed 's/\/testgetapi/\/test\//g' azure-apim.jmx > azure-apim.jmx
-#sed 's/a9fecf7d693641ccb57211f2720405c6/g' > azure-apim.jmx
+# Check the Azure API pointing to Azure Containers
+export APIHOST="https://perf-poc-eastus2-01.regional.azure-api.net/local"
+export APIHEADER="Ocp-Apim-Subscription-Key: a9fecf7d693641ccb57211f2720405c6"
+curl $APIHOST --header "${APIHEADER}"
+# Repeat and show response time.
+curl -o /dev/null -s -w '%{http_code} in %{time_total}s\n' $APIHOST --header "${APIHEADER}"
 
-jmeter -JURLHOST=perf-poc-eastus2-01.regional.azure-api.net -JURLPATH=/test2f/ -JHEADERNAME=Ocp-Apim-Subscription-Key -JHEADERVALUE=a9fecf7d693641ccb57211f2720405c6 -n -t apigee.jmx -o output/apigee -l output/apigee.jtl -j output/apigee.log -e
+# Test the Apigee prod env service pointing to Google Cloud Run
+#jmeter -JURLHOST=geico-prod.apigee.net -JURLPATH=/testgetapi \
+#  -JHEADERNAME=x-apikey -JHEADERVALUE=upYgIDoREuHk3XGXeH8DweIN3J4ZfDQ5 \
+#  -n -t webtestplan.jmx -o output/apigee-gcp -l output/apigee-gcp.jtl \
+#  -j output/apigee-gcp.log -e
 
-#cat output/apigee/statistics.json
+# Test the Azure APIM API that points to Google Cloud Run
+#jmeter -JURLHOST=perf-poc-eastus2-01.regional.azure-api.net -JURLPATH=/test/ \
+#  -JHEADERNAME=Ocp-Apim-Subscription-Key -JHEADERVALUE=a9fecf7d693641ccb57211f2720405c6 \
+#  -n -t webtestplan.jmx -o output/azure-apim-gcp -l output/azure-apim-gcp.jtl \ 
+#  -j output/azure-apim-gcp.log -e
 
-#jmeter -n -t azure-apim.jmx -o output/azure-apim -l output/azure-apim.jtl -j output/azure-apim.log -e
+# Test the Apigee prod env service pointing to Azure Containers
+jmeter -JURLHOST=geico-prod.apigee.net -JURLPATH=/hello-from-gates \
+  -JHEADERNAME=x-apikey -JHEADERVALUE=upYgIDoREuHk3XGXeH8DweIN3J4ZfDQ5 \
+  -n -t webtestplan.jmx -o output/apigee-ms -l output/apigee-ms.jtl \
+  -j output/apigee-ms.log -e
 
-#cat output/azure-apim/statistics.json
+# Test the Azure APIM API that points to Azure Containers
+jmeter -JURLHOST=perf-poc-eastus2-01.regional.azure-api.net -JURLPATH=/local/ \
+  -JHEADERNAME=Ocp-Apim-Subscription-Key -JHEADERVALUE=a9fecf7d693641ccb57211f2720405c6 \
+  -n -t webtestplan.jmx -o output/azure-apim-ms -l output/azure-apim-ms.jtl \
+  -j output/azure-apim-ms.log -e
+
+
+echo "*** Apigee to GCP ***"
+cat output/apigee-gcp/statistics.json
+
+echo "*** Azure APIM to GCP ***"
+cat output/azure-apim-gcp/statistics.json
+
+
+echo "*** Apigee to Azure Containers ***"
+cat output/apigee-ms/statistics.json
+
+echo "*** Azure APIM to Azure Containers ***"
+cat output/azure-apim-ms/statistics.json
